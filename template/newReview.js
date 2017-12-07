0faede893caf0979873b98e811801d2cd2a761c4
Lena Venable <venablena@gmail.com>
2017-12-07 15:11:29 -0800

    working on add a review

--- template/newReview.js
+++ template/newReview.js
@@ -5,27 +5,28 @@ function reviewForm(snack_id){
           <div class="modal-card">
           <div id="error-message">
           </div>
+          <form class="review">
           <div class="field">
           <div class="control">
           <label class="label" id="rating">Rating</label>
             <label class="radio">
-              <input type="radio" name="question">
+              <input type="radio" name="question" class="rating" value="1">
               1
             </label>
             <label class="radio">
-              <input type="radio" name="question">
+              <input type="radio" name="question" class="rating" value="2">
               2
             </label>
             <label class="radio">
-              <input type="radio" name="question">
+              <input type="radio" name="question" class="rating" value="3" checked>
               3
             </label>
             <label class="radio">
-              <input type="radio" name="question">
+              <input type="radio" name="question" class="rating" value="4">
               4
             </label>
             <label class="radio">
-              <input type="radio" name="question">
+              <input type="radio" name="question" class="rating" value="5">
               5
             </label>
           </div>
@@ -46,6 +47,8 @@ function reviewForm(snack_id){
             <button id="${snack_id}"class="button is-info review-submit">Submit</button>
             <button class="delete is-large" id="close-modal" aria-label="close"></button>
           </div>
+
+          </form>
         </div>
       </div>`
 }
